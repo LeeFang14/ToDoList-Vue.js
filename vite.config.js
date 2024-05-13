7e3bc497fb182f4430d0679b996ebe1885a15081
Fang Lee <fcl411991@gmail.com>
2024-05-14 06:42:06 +0800

    Fix: url base path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/ToDoList-Vue/',
+  base: '/ToDoList-Vue.js/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
